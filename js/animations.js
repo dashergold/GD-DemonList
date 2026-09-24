b6b0c4dacda8a9cd7c356b4c3730f3ad947e86e1
axel <axel.s.kallen@gmail.com>
2026-09-24 22:09:38 +0200

    animation transition fix in main major rework to the document header on every page

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,5 @@
+
+//adds the show class to elements when they enter view and removes it when they leave
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         console.log(entry);
@@ -10,6 +12,24 @@ const observer = new IntersectionObserver(entries => {
     });
 });
 
-
+//adds observer to all elements that arent visible 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+const header = document.querySelector(".site-header");
+const menuToggle = document.querySelector(".menu-toggle");
+
+//when user scrolls more than 50px add scrolled class to the header, otehrwise remove scrolled class, and menu open class if the burger menu is opened
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+        header.classList.remove("menu-open");
+    }
+});
+
+//when the burger menu is clicked it adds open menu to the header which opens a dropdown menu to display the nav
+menuToggle.addEventListener("click", () => {
+    header.classList.toggle("menu-open");
+});
